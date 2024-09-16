7b91666303f7e0769b3659972264426702c1f147
Efraim Kristhianno <ekriator@gmail.com>
2024-09-16 17:24:28 -0300

    att

--- LangChain.py
+++ LangChain.py
@@ -1,5 +1,5 @@
 import os
-import streamlit as st
+#import streamlit as st
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
