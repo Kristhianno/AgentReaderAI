9a2b3c7897c06c031b2ce6a6734e9bbd03622b70
Efraim Kristhianno <ekriator@gmail.com>
2024-10-03 13:24:43 -0300

    att

--- ReaderTXT.py
+++ ReaderTXT.py
@@ -1,25 +1,57 @@
 import os
 from crewai import Agent, Task, Crew, Process
 from langchain_groq import ChatGroq
-from crewai_tools import FileReadTool
-from dotenv import load_dotenv
+from crewai_tools import FileReadTool, ScrapeWebsiteTool
 
+from dotenv import load_dotenv
 load_dotenv()
+
 
 GroqApiKey = os.getenv('GROQ_API_KEY')
 
-
-
-llama3 = ChatGroq(
-            api_key= GroqApiKey,
-            model="groq/llama3-70b-8192"
-        )
-
+llama3 = ChatGroq( api_key= GroqApiKey, model="groq/llama3-70b-8192")
 
 
 file_read_tool = FileReadTool(file_path= 'tecnicasVendas.txt')
+WebScraping =ScrapeWebsiteTool(website_url= 'https://www.google.com')
 
 
+
+Vendedor = Agent(
+    role='Vendedor Comercial',
+    goal='Pesquisar na internet os clientes e fornecedores que utilizam nosso produto.Retorne as mensagens em Português do Brasil',
+    backstory="O vendedor é um profissional inteligente e comunicador,retorne as mensagens em Português do Brasil",
+    verbose= True,
+    llm=llama3
+)
+
+
+Trazer_informacao = Task(
+    description="O Vendedor deve trazer informações precisas para o Analista , retorne as mensagens em Português do Brasil.",
+    expected_output = "Todas as informações foram passadas para o Analista, caminho livre para trazer as melhores estratégias.",
+    tools= [WebScraping],
+    agent=Vendedor
+)
+
+
+
+Analista = Agent(
+    role='Analista de Dados',
+    goal=  "Analisar os dados do arquivo e pontuar quais são as estratégias mais usadas no mundo corporativo" 
+           "retorne as mensagens em Português do Brasil",
+    backstory="O Analista é um profissinal altamente requisitado para o time de negócio, muito qualificado com PHD no MIT , retorne  as mensagens em Português do Brasil.",
+    verbose= True,
+    llm=llama3
+)
+
+
+Trazer_analises_precisas = Task(
+    description= "O Analista deve fornecer análises precisas de acordo com a pesquisa feita pelo vendedor, obtendo insights valiosos." 
+                 "retorne as mensagens em Português do Brasil",
+    tools = [file_read_tool],
+    expected_output="Análise conclúida com sucesso e encaminhada para o Diretor.",
+    agent = Analista
+)
 
 
 Diretor = Agent(
@@ -32,68 +64,20 @@ Diretor = Agent(
     )
 
 
-
-Analista = Agent(
-    role='Analista de Dados',
-    goal= "Analisar os dados do arquivo e pontuar quais são os top 10 produtos com mais estoque" 
-            " disponível para venda,retorne as mensagens em Português do Brasil",
-    tools=[file_read_tool],
-    backstory="O Analista é um profissinal altamente requisitado para o time de negócio, muito qualificado com PHD no MIT , retorne  as mensagens em Português do Brasil.",
-    verbose= True,
-    llm=llama3
-)
-
-
-
-Vendedor = Agent(
-    role='Vendedor Comercial',
-    goal='Pesquisar na web as melhores notícias para o analista possa identificar correlações entre elas e o documento.Retorne as mensagens em Português do Brasil',
-    tools=[file_read_tool],
-    backstory="O vendedor é um profissional inteligente e comunicador,retorne as mensagens em Português do Brasil",
-    verbose= True,
-    llm=llama3
-)
-
-
-
-
-
-
 coordenar_equipe = Task(
     description= " O Diretor deve coordenar a equipe, mantendo a comunicação e fornecendo suporte estratégico."
                  " Diretor deve ordenar primeiro que o Vendedor pesquise na web para que o Analista, analise o documento " 
                  " e possa trazer informações relevantes, retorne as mensagens em Português do Brasil" ,
 
     expected_output="Análise bem sucedida, temos a estratégia perfeita para a diretoria. Retorne as mensagens em Português do Brasil.",
-    agent=Diretor,
-    allow_delegation=True
+    agent=Diretor
 )
-
-
-
-
-Trazer_informacao = Task(
-    description="O Vendedor deve trazer informações precisas para o Analista , retorne as mensagens em Português do Brasil.",
-    expected_output="Todas as informações foram passadas para o Analista, caminho livre para trazer as melhores estratégias.",
-    agent=Vendedor
-)
-
-
-
-Trazer_analises_precisas = Task(
-    description= "O Analista deve fornecer análises precisas do documento que venho do Vendedor obtendo insights valiosos." 
-                 "retorne as mensagens em Português do Brasil",
-
-    expected_output="Análise conclúida com sucesso.",
-    agent=Analista
-)
-
 
 
 # Inicialização da equipe
 time_comercial = Crew(
-    agents=[Diretor, Vendedor, Analista],
-    tasks=[coordenar_equipe, Trazer_informacao, Trazer_analises_precisas],
+    agents=[Vendedor, Analista, Diretor],
+    tasks=[Trazer_informacao, Trazer_analises_precisas, coordenar_equipe],
     process=Process.hierarchical,
     manager_llm=llama3
 )
